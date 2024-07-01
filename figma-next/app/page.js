e25e27241287b6aadadbc6ef1f4fa9baf4b02ab6
LuMenorAP <lucas.hess@bue.edu.ar>
2024-07-01 10:13:45 -0300

    primer commit

---
+++ figma-next/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <>
+      <Button type="primary">Button</Button>
+    </>
+  );
+}
